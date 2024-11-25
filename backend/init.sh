b1f6d7f8d2ad8bf29d1ef3666ee06cc5927622bc
Yangjin Cho <choyj1222@gmail.com>
2024-11-25 11:04:21 +0900

    feat: init.sh에서 docker 명령어에 sudo 추가

--- backend/init.sh
+++ backend/init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker pull postgres
+sudo docker pull postgres
 
-docker run --name drizzle-postgres -e POSTGRES_PASSWORD=3942 -d -p 5432:5432 postgres
+sudo docker run --name drizzle-postgres -e POSTGRES_PASSWORD=3942 -d -p 5432:5432 postgres
 
 touch .env && echo "DATABASE_URL=postgres://postgres:3942@localhost:5432/postgres" > .env
 
